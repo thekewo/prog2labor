ae2ee9a0f3552ec1b54670a013cca3354c022e09
Koós Zoltán <koos.zoltan.iso@gmail.com>
2018-09-19 09:30:18 +0200

    Add files via upload

---
+++ prog2/Gagyi/Gagyi2.java
@@ -0,0 +1,18 @@
+
+public class Gagyi2
+{
+
+public static void main (String[]args)
+{
+
+Integer x = -129;
+Integer t = -129;
+
+System.out.println (x);
+System.out.println (t);
+
+while (x <= t && x >= t && t != x);
+ 
+}
+
+}
